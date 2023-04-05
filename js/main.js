08ed2a952b0a666c3136e41a41be3a69112224e5
Dave <wrobel.dawid.p@gmail.com>
2023-04-05 22:13:34 +0200

    added section about

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 const burgerBtn = document.querySelector('.burger-btn');
 const navMobile = document.querySelector('.nav__mobile');
 const allMobileNavItems = document.querySelectorAll('.nav__mobile-link');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
 	navMobile.classList.toggle('nav__mobile--active');
+    document.body.classList.toggle('sticky-body');
 
 	allMobileNavItems.forEach((item) => {
 		item.addEventListener('click', () => {
 			navMobile.classList.remove('nav__mobile--active');
+            document.body.classList.remove('sticky-body');
 		});
 	});
-    
+
 };
 
+
+
+
+const hanleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+hanleCurrentYear();
 burgerBtn.addEventListener('click', handleNav);
